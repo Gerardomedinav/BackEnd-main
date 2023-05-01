4950d5c1a4d874f89067d78bbfd13f4025b26714
Gerardomedinav <gerardomedinav@hotmail.es>
2023-04-30 23:40:57 -0300

    agregar dependencia
    <dependency>
        <groupId>org.springframework.boot</groupId>
        <artifactId>spring-boot-starter-web</artifactId>
    </dependency>

--- src/main/java/com/portfolio/gm/Security/MainSecurity.java
+++ src/main/java/com/portfolio/gm/Security/MainSecurity.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.portfolio.gm.Security;
 
 import com.portfolio.gm.Security.Service.UserDetailsImpl;
